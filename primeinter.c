2d3bede0e32778d3243a96f40974ef3d494c7341
Arunthathi <arunthi19@gmail.com>
2016-07-09 02:24:30 -0700

    Create primeinter.c

---
+++ primeinter.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int start,end,i,flag=0,x;
+printf("enter the starting and ending interval");
+scanf("%d%d",&start,&end);
+for(i=start;i<=end;i++)
+{
+  for(x=2;x<i;x++)
+{
+if(i%x==0)
+{
+flag=1;
+}
+}
+if(flag==0)
+printf("%d",&i);
+}
+return 0;
+}
